65188fcf9ff3372d96dd004a842f859d3da7ccd1
mohibuslc <mohib_uslc@outlook.com>
2021-03-26 00:35:58 +0000

    Shop goods view on webpage

--- src/Components/Header/Shop/Shop.js
+++ src/Components/Header/Shop/Shop.js
@@ -1,13 +1,31 @@
-import React, { Component } from 'react'
 
-export class Shop extends Component {
-    render() {
-        return (
-            <div>
-                <h1> This Is Go Shop</h1>
-            </div>
-        )
-    }
-}
 
-export default Shop
+import React from 'react'; //@ mandotory@ for this line declare about this page under the react:
+import fakeData from '../../../fakeData'; // for this  line declare to react have one fakeData:
+import {useState} from 'react'; // for this line declare to useState:
+
+
+
+const Shop = () => {
+
+    const first10 = fakeData.slice(5,20); // 1st 10 data 
+
+    const [products, setProducts] = useState(first10);
+
+    return (
+        <div>
+            <h1>This All Product Are abulavale</h1>
+
+            <h3>{products.length}</h3>
+            <ul>
+                {
+
+                    products.map(pd => <li>{pd.name}</li>)
+                }
+                </ul>
+            
+        </div>
+    );
+};
+
+export default Shop;
